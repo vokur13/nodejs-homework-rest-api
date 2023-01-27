e4ce0f8582d58942ff52a3b1be8abd9a5d53fb9f
Volodymyr Kurov <vokur13@gmail.com>
2023-01-27 19:27:12 +0200

    Update dataModel.js

--- src/db/dataModel.js
+++ src/db/dataModel.js
@@ -1,22 +1,25 @@
 const mongoose = require('mongoose');
 
-const dataSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
+const dataSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    unique: true,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 const Contact = mongoose.model('contact', dataSchema);
 
