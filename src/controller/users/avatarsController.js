aaaaa29ee1c7f7e30e95326a05519299157d9a5e
Volodymyr Kurov <vokur13@gmail.com>
2023-02-18 08:08:24 +0200

    avatarUpdateController fixed

--- src/controller/users/avatarsController.js
+++ src/controller/users/avatarsController.js
@@ -4,28 +4,37 @@ const uploadDir = path.resolve('public/avatars');
 const Jimp = require('jimp');
 const mime = require('mime-types');
 const { v4: uuidv4 } = require('uuid');
+
 const { userAvatarUpdate } = require('../../service');
 
 const avatarUpdateController = async function (req, res) {
-  const { description } = req.body;
   const { path, mimetype } = req.file;
   const { _id } = req.user;
   const uniqueSuffix = uuidv4();
   const ext = mime.extension(mimetype);
   const staticFileName = `${_id}-${uniqueSuffix}.${ext}`;
 
-  Jimp.read(path, (err, avatar) => {
-    if (err) throw err;
-    avatar
-      .resize(250, 250) // resize
-      .write(`${uploadDir}/${staticFileName}`); // save
-  });
+  try {
+    Jimp.read(path, (err, avatar) => {
+      if (err) throw err;
+      avatar
+        .resize(250, 250) // resize
+        .write(`${uploadDir}/${staticFileName}`); // save
+    });
 
-  await userAvatarUpdate(_id, staticFileName);
+    await userAvatarUpdate(_id, staticFileName);
 
-  await fs.unlink(path);
-
-  res.json({ description, message: 'File uploaded successfully', status: 200 });
+    res.status(200).json({
+      status: 200,
+      message: 'File uploaded successfully',
+      avatarURL: `${uploadDir}/${staticFileName}`,
+      // avatarURL: `/avatars/${staticFileName}`,
+    });
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    await fs.unlink(path);
+  }
 };
 
 module.exports = {
